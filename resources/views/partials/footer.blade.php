48b44d2252273778ada572c3bfeb8d6363a8e283
Summiya <summiya.rasheed@gmail.com>
2023-08-26 19:52:30 +0400

    front end updates

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-light text-center text-lg-start" style="margin-top: 50px;">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js"
+        integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
